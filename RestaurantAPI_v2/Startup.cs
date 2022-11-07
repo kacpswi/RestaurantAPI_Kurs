168804482daacf91e7e2fc2ad55b180cdc166883
Kacper Świątek <kacperswiatek99@gmail.com>
2022-11-07 19:16:03 +0100

    removing comment in startup class

--- RestaurantAPI_v2/Startup.cs
+++ RestaurantAPI_v2/Startup.cs
@@ -75,7 +75,7 @@ namespace RestaurantAPI_v2
 
         }
 
-        //Some comment
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
